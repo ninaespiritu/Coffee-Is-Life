a96beb6e1baae47d5417a960956aa949707e54c8
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-28 14:43:23 +0000

    Add signup functionality

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,21 @@
+export const Signup = ({ props }) => {
+	return (
+		<div>
+			<form onSubmit={props.handleSignup}>
+				<input
+					onChange={(e) => props.setEmail(e.target.value)}
+					placeholder="Email"
+				/>
+				<input
+					onChange={(e) => props.setUsername(e.target.value)}
+					placeholder="Username"
+				/>
+				<input
+					onChange={(e) => props.setPassword(e.target.value)}
+					placeholder="Password"
+				/>
+				<button type="submit">Sign Up</button>
+			</form>
+		</div>
+	);
+};
---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+export const fetchSignup = async (setUser, email, username, password) => {
+	try {
+		const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email: email,
+				username: username,
+				password: password,
+			}),
+		});
+		const data = await response.json();
+		console.log(data);
+		setUser(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
