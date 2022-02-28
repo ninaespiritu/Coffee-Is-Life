182a8d1e366f4a9fa5f6b050aa8f383075ee2ae6
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-28 14:54:38 +0000

    Fix CORS error in signup

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,7 @@ export const fetchSignup = async (setUser, email, username, password) => {
 	try {
 		const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
 			method: "POST",
+			mode: "no-cors",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
 				email: email,
