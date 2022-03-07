bcf4eaef4e484718cec187374334919ceb3ed5fb
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-03-07 14:12:49 +0000

    Style shop details page

--- src/components/ShopDetails/ShopDetails.js
+++ src/components/ShopDetails/ShopDetails.js
@@ -55,34 +55,67 @@ export const ShopDetails = ({ shops, shopNum, user }) => {
 	};
 
 	return (
-		<div>
+		<div className="shopdetails">
 			{shop && (
 				<div className="shop">
-					<h4>Shop Details</h4>
-					<h1>{shop.name}</h1>
-					<h3>{shop.location}</h3>
-					<p>{shop.description}</p>
-
-					<div className="shop-img">
-						<img src={shop.url} alt="" />
+					<div className="shop-header">
+						<div className="shop-header-text">
+							<h4>Shop Details</h4>
+							<h1>{shop.name}</h1>
+						</div>
 					</div>
-					<ShopReview shops={shops} shopNum={shopNum} user={user} />
-					<div>
-						<h2>Reviews</h2>
-						{reviews.map((review) => (
-							<div key={review._id}>
-								<h4>{review.username}</h4>
-								<p>{review.text}</p>
-								<p>Rating: {review.rating} out of 10</p>
+
+					<div className="shop-info">
+						<div className="shop-info-box">
+							<div className="shop-text">
+								<h4>Location</h4>
+								<h3>{shop.location}</h3>
+								<h4>Description</h4>
+								<p>{shop.description}</p>
 							</div>
-						))}
-						{newReviews.map((review) => (
-							<div key={review._id}>
-								<h4>{review.username}</h4>
-								<p>{review.text}</p>
-								<p>Rating: {review.rating} out of 10</p>
+
+							<div className="shop-img">
+								<img src={shop.url} alt="" />
 							</div>
-						))}
+						</div>
+					</div>
+
+					<div className="reviews">
+						<div className="reviews-box">
+							<div className="add-review">
+								<ShopReview
+									shops={shops}
+									shopNum={shopNum}
+									user={user}
+								/>
+							</div>
+
+							<div className="list-review">
+								<h2>Reviews</h2>
+								{reviews.map((review) => (
+									<div key={review._id} className="review">
+										<h4>{review.username}</h4>
+										<p>
+											<span>
+												Rating: {review.rating}/10
+											</span>
+										</p>
+										<p>{review.text}</p>
+									</div>
+								))}
+								{newReviews.map((review) => (
+									<div key={review._id} className="review">
+										<h4>{review.username}</h4>
+										<p>
+											<span>
+												Rating: {review.rating}/10
+											</span>
+										</p>
+										<p>{review.text}</p>
+									</div>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
 			)}
